3f38e080ff3a80527ea9540e0bd122df32f06b9e
Aghent47 <neiderkk@gmail.com>
2024-01-27 21:34:48 -0500

    Validar campos obligatorios - Email

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 import { response, request } from "express";
 import bcryptjs from 'bcryptjs';
 import {User} from '../models/user.js';
+import { validationResult } from "express-validator";
 
 const usuarios = {};
 
@@ -17,16 +18,29 @@ usuarios.get = (req = request, res = response) => {
 
 usuarios.post = async (req, res) => {
 
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors);
+    }
+    
     const {name, mail, password, rol} = req.body;
 
     // const body = req.body;
     const usuario = new User({name, mail, password, rol});
+    
     // verificar si el correo existe
+    const existeEmail = await User.findOne({mail});
+    if(existeEmail){
+        return res.status(400).json({
+            msg:'El correo ya está registrado!!'
+        });
+    }
 
     // Encriptar la contraseña HASS
     const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync(password, salt);
-
+    
     // Guardar en BD
     await usuario.save();
     res.json({
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import usuarios from '../controllers/users.js';
+import { check } from 'express-validator';
 const router = express.Router();
 
 router.get('/', usuarios.get);
-router.post('/', usuarios.post);
+
+router.post('/',[
+    check('mail', 'El correo no es válido').isEmail(),
+], usuarios.post);
+
 router.put('/:id', usuarios.put);
 router.delete('/', usuarios.delete);
 router.patch('/', usuarios.patch);
