704862569d6cb0cfa1f7a29c0b7701a9571abd65
Aghent47 <neiderkk@gmail.com>
2024-02-14 23:32:21 -0500

    . Actualizar imagen de usuario y producto

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,8 @@
 import { response } from "express";
 import { subirArchivo } from "../helpers/subir-archivo.js";
 
+import { User, Producto } from "../models/index.js";
+
 export const cargarArchivo = async (req, res = response) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
@@ -25,8 +27,37 @@ export const actualizarImagen = async (req, res = response) => {
 
     const { coleccion, id } = req.params;
 
+    let modelo;
+
+    switch (coleccion) {
+        case 'users':
+
+            modelo = await User.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un Producto con el id ${id}`
+                })
+            }
+            break;
+        default:
+            return res.status(500).json({ msg: 'Se me olvido validar esto' });
+    }
+
+    //grabas en BD
+    const pathFile = await subirArchivo(req.files, undefined, coleccion);
+    modelo.img = pathFile;
+
+    await modelo.save();
+
     res.json({
-        coleccion,
-        id
+        modelo
     });
 }
